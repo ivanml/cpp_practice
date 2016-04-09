48062fabc61ae98c1ab47fd53ee95ccb36ebb197
Lei Mao <ivan@Leis-MBP.home>
2016-04-09 11:09:18 -0400

    a more robust quicksort and partition

--- main.cxx
+++ main.cxx
@@ -4,10 +4,7 @@
 using namespace std;
 
 int main() {
-  // easier way to initialize a vector
-  static const int arr[] = {16,2,77,29};
-  vector<int> nums(arr, arr + sizeof(arr) / sizeof(arr[0]) );
-
+  vector<int> nums = {1, 2, 3, 4, 5};
   print(nums);
 
   return 0;
--- quicksort.cxx
+++ quicksort.cxx
@@ -3,44 +3,42 @@
 
 using namespace std;
 
-int partition(vector<int> &nums, int i, int j) {
-  int pivot = nums[i + (j - i) / 2];
+int partition(vector<int> &nums, int left, int right) {
+  int pivot = nums[left];
+  int l = left + 1; // starting from second element
+  int r = right;
 
-  while (i <= j) {    
-    while (nums[i] < pivot)
-      i ++;
-    while (nums[j] > pivot)
-      j --;
+  while (l <= r) {
+    if (nums[l] > pivot && nums[r] < pivot)
+      swap(nums[l++], nums[r--]);
     
-    if (i <= j) {
-      // swap
-      int t = nums[i];
-      nums[i] = nums[j];
-      nums[j] = t;
-      i ++;
-      j --;
-    }
+    while (l <= r && nums[l] <= pivot) l ++;
+    while (l <= r && nums[r] >= pivot) r --;
   }
-
-  return i;
+  
+  // put pivot in its correct pos, which is r
+  swap(nums[r], nums[left]);
+  return r;
 }
 
-void quicksort(vector<int> &nums, int i, int j) {
-  if (nums.empty() || i >= j)
-    return;
+void quicksort(vector<int> &nums, int left, int right) {
+  if (left < right) {
+    int pivotIndex = partition(nums, left, right);
 
-  int index = partition(nums, i, j);
-  if (index > i)
-    quicksort(nums, i, index - 1);
-  if (index < j)
-    quicksort(nums, index, j);
+    if (pivotIndex - 1 > left)
+      quicksort(nums, left, pivotIndex - 1);
+    if (pivotIndex + 1 < right)
+      quicksort(nums, pivotIndex + 1, right);
+  }
 }
 
 int main() {
-  vector<int> nums = {4, 1, 6, 3, 5, 7, 2};
+  vector<int> nums = {8, 3, 1, 5, 4, 2, 7, 6, 9, 5, 1, 2, 6, 4, 0, 0, 1, 4, 5, 5, 5, 3, 2, 9};
+  cout << "input nums: ";
   print(nums);
 
   quicksort(nums, 0, nums.size() - 1);
+  cout << "sorted nums: ";
   print(nums);
 
   return 0;
